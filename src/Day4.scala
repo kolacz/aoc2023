737bd4853e5be463141f4c1b01f0a14569d5f53d
kolacz <andrzejkolacz@icloud.com>
2023-12-09 22:06:22 +0000

    Completed the part two of Day4

--- src/Day4.scala
+++ src/Day4.scala
@@ -17,11 +17,25 @@ def firstSolutionDay4(): Unit =
       ScratchCard(winningNumbers, numbersYouHave) <- lines.map(ScratchCardParser(_))
       matches = winningNumbers.count(numbersYouHave.contains) if matches > 0
     yield math.pow(2, matches - 1)
-
     println(points.sum.toInt)
 
-def secondSolutionDay4(): Unit = ???
+case class DuplicativeCard(numberOfInstances: Int, remainingCopiesToCreateBelow: Int)
+
+def secondSolutionDay4(): Unit =
+  project.withInputOfDay(4): lines =>
+    val cardInstances = lines.foldLeft(List.empty[DuplicativeCard]):
+      case (previousCards, line) =>
+        val ScratchCard(winningNumbers, numbersYouHave) = ScratchCardParser(line)
+        val matchesCount = winningNumbers.count(numbersYouHave.contains)
+        val currentCardInstances = 1 + previousCards.filter(_.remainingCopiesToCreateBelow > 0).map(_.numberOfInstances).sum
+        val previousCardsApplied = previousCards.map: card =>
+          DuplicativeCard(card.numberOfInstances, card.remainingCopiesToCreateBelow - 1)
+
+        DuplicativeCard(currentCardInstances, matchesCount) :: previousCardsApplied
+
+    println(cardInstances.map(_.numberOfInstances).sum)
 
 @main
 def day4(): Unit =
   firstSolutionDay4()
+  secondSolutionDay4()
